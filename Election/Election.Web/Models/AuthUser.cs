e23ce40cac5f4aa42e0b596605584c26880c1ca3
astakgowda <astakgowda@yahoo.co.in>
2015-10-19 10:46:59 +0530

    added new project

---
+++ Election/Election.Web/Models/AuthUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Election.Web
+{
+	public class AuthUser
+	{
+		public AuthUser ()
+		{
+		}
+
+		public string LoginName{get;set;}
+		public int Password{ get; set;}
+	}
+}
+
